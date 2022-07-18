d5ceb3342428ad9b5b1cabac95f197bc445b0a6c
CanonOverseer <37056700+CanonOverseer@users.noreply.github.com>
2022-07-19 00:46:21 +0800

    Create TestMSG.cs

---
+++ Source/TitularRoyalty/TestMSG.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace TitularRoyalty.Verbs
+{
+    [StaticConstructorOnStartup]
+    public static class TestMsg
+    {
+        static TestMsg()
+        {
+            Log.Message("Hello There");
+        }
+    }
+}
