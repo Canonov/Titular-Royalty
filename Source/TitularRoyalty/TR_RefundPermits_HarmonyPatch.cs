8676d356e48ee2957e8b4fe18e8a40af9c6a0601
CanonOverseer <37056700+CanonOverseer@users.noreply.github.com>
2022-07-25 13:45:25 +0800

    Create TR_RefundPermits_HarmonyPatch.cs

---
+++ Source/TitularRoyalty/TR_RefundPermits_HarmonyPatch.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using Verse;
+using HarmonyLib;
+
+namespace TitularRoyalty
+{
+
+    [HarmonyPatch(typeof(Pawn_RoyaltyTracker), nameof(Pawn_RoyaltyTracker.RefundPermits))]
+    class TRPermitPatch
+    {
+        static void Prefix(ref Pawn ___pawn, ref List<FactionPermit> ___factionPermits, int favorCost, Faction faction)
+        {
+            ___pawn.royalty.GainFavor(faction, 20);
+            ___factionPermits.Clear();
+
+            Log.Message($"From Harmony {___pawn.royalty.GetCurrentTitleInFaction(faction)}");
+            return;
+        }
+    }
+
+}
