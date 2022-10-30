bcb32edb5bc9a4f801dbe10aa53242c650aa49e6
CanonOverseer <37056700+CanonOverseer@users.noreply.github.com>
2022-10-30 22:20:21 +0800

    Create Resources.cs

---
+++ Source/TitularRoyalty/Miscellaneous/Resources.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TitularRoyalty
+{
+    public enum TitleTiers : int
+    {
+        Lowborn = 0,
+        Gentry = 1,
+        LowNoble = 2,
+        HighNoble = 3,
+        Royalty = 4,
+        Sovereign = 5,
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Resources
+    {
+        public static readonly Texture2D CrownIcon;
+        public static readonly List<Texture2D> TitleTierIcons;
+
+        static Resources()
+        {
+            CrownIcon = ContentFinder<Texture2D>.Get("UI/Gizmos/givetitleicon");
+
+            //Todo add custom tier icons
+            var titletierplaceholder = ContentFinder<Texture2D>.Get("UI/Gizmos/givetitleicon");
+            TitleTierIcons = new List<Texture2D>();
+            for (int i = 0; i <= 5; i++)
+            {
+                TitleTierIcons.Add(titletierplaceholder);
+            }
+        }
+    }
+}
