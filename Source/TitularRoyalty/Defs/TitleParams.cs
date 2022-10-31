1a33e0324f8ce1bca102de91bd7ad1e1509c3e09
CanonOverseer <agent6841@gmail.com>
2022-10-31 11:38:48 +0800

    Create TitleParams.cs

---
+++ Source/TitularRoyalty/Defs/TitleParams.cs
@@ -0,0 +1,18 @@
+﻿//using System; 
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TitularRoyalty
+{
+    public class RealmTitle
+    {
+        [NoTranslate]
+        public string realmType;
+
+        public string label;
+        public string labelFemale = "none";
+    }
+}
