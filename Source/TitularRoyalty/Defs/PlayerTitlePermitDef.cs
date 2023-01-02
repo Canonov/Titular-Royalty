51075a542ade9fcdf1f2f35180579c4eee8eb196
CanonOverseer <agent6841@gmail.com>
2023-01-03 02:45:50 +0800

    Patches so permits work

---
+++ Source/TitularRoyalty/Defs/PlayerTitlePermitDef.cs
@@ -0,0 +1,14 @@
+﻿//using System; 
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TitularRoyalty
+{
+    public class PlayerTitlePermitDef : RoyalTitlePermitDef
+    {
+        
+    }
+}
