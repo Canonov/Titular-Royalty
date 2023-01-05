28cf1aafb5bbfac1f0ff3354beaa75393d22b156
CanonOverseer <agent6841@gmail.com>
2023-01-05 14:21:47 +0800

    RealmType Rewrite

---
+++ Source/TitularRoyalty/Defs/RealmTypeDefOf.cs
@@ -0,0 +1,48 @@
+﻿ using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TitularRoyalty
+{
+
+    [DefOf]
+    public class RealmTypeDefOf
+    {
+        static RealmTypeDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(RealmTypeDefOf));
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class BaseRealmType
+    {
+        public static RealmTypeDef Def;
+        
+        static BaseRealmType()
+        {
+            Def = new RealmTypeDef()
+            {
+                defName = "RealmTypeBase",
+                label = "Base Realm Type",
+            };
+
+            Def.titleOverrides = new List<RealmTypeTitle>();
+            
+            foreach(PlayerTitleDef title in DefDatabase<PlayerTitleDef>.AllDefsListForReading)
+            {
+                Def.titleOverrides.Add(new RealmTypeTitle()
+                {
+                    titleDef = title,
+
+                    label = title.label,
+                    labelFemale = title.labelFemale,
+                    tierOverride = title.titleTier
+                    
+                });
+            }
+        }
+    }
+}
