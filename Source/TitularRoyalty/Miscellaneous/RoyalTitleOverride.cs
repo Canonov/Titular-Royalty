b5933a10068b64d4511723b4ec568bc7201f77bf
CanonOverseer <agent6841@gmail.com>
2023-02-19 10:28:09 +0800

    Scribing for RoyalTitleOverride

--- Source/TitularRoyalty/Miscellaneous/RoyalTitleOverride.cs
+++ Source/TitularRoyalty/Miscellaneous/RoyalTitleOverride.cs
@@ -14,8 +14,9 @@ namespace TitularRoyalty
         public string labelFemale = "None";
 
         public bool inheritable = false;
-        public bool useTierOverride = false;
+        public bool useTierOverride = false; // Realmtype only
         public TitleTiers titleTier = TitleTiers.Lowborn;
+
         public ExpectationDef minExpectation;
 
         public RoyalTitleOverride() { }
@@ -45,8 +46,14 @@ namespace TitularRoyalty
 
         public void ExposeData()
         {
+            Scribe_Defs.Look(ref titleDef, nameof(titleDef));
+
             Scribe_Values.Look(ref label, nameof(label), "None");
             Scribe_Values.Look(ref labelFemale, nameof(labelFemale), "None");
+
+            Scribe_Values.Look(ref inheritable, nameof(inheritable), false);
+            Scribe_Values.Look(ref titleTier, nameof(titleTier), TitleTiers.Lowborn);
+            Scribe_Defs.Look(ref minExpectation, nameof(minExpectation));
         }
     }
 }
