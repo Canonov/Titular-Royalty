fc66f3d9ba539b94500a9a085d936877aaf9ab43
CanonOverseer <agent6841@gmail.com>
2023-06-22 17:38:40 +0800

    Update Comp_ManageTitles.cs

--- Source/TitularRoyalty/Components/Comp_ManageTitles.cs
+++ Source/TitularRoyalty/Components/Comp_ManageTitles.cs
@@ -14,25 +14,24 @@ namespace TitularRoyalty
 
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
-            foreach (Gizmo item in base.CompGetGizmosExtra())
-            {
-                yield return item;
-            }
+	        foreach (var gizmo in base.CompGetGizmosExtra())
+	        {
+		        yield return gizmo;
+	        }
 
-            var manageTitlesGizmo = new Command_Action
-            {
-	            icon = ContentFinder<Texture2D>.Get(Props.managetitlesIconPath),
-	            defaultLabel = "TR_Command_managetitles_label".Translate(),
-	            defaultDesc = "TR_Command_managetitles_desc".Translate(),
-	            action = delegate
-	            {
-		            var window = new Dialog_ManageTitles();
-		            Find.WindowStack.Add(window);
-	            }
-            };
+	        var manageTitlesGizmo = new Command_Action
+	        {
+		        icon = ContentFinder<Texture2D>.Get(Props.managetitlesIconPath),
+		        defaultLabel = "TR_Command_managetitles_label".Translate(),
+		        defaultDesc = "TR_Command_managetitles_desc".Translate(),
+		        action = delegate
+		        {
+			        var window = new Dialog_ManageTitles();
+			        Find.WindowStack.Add(window);
+		        }
+	        };
 
-            yield return manageTitlesGizmo;
-
+	        yield return manageTitlesGizmo;
         }
 
         public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
@@ -47,33 +46,35 @@ namespace TitularRoyalty
             }, itemIcon: Resources.CrownIcon, iconColor: Color.white);
 
             //Set Heir Option
-            if (TitularRoyaltyMod.Settings.inheritanceEnabled && (selPawnTitle = selPawn.royalty?.GetCurrentTitleInFaction(Faction.OfPlayer)?.def) != null && selPawnTitle.canBeInherited )
+            if (TitularRoyaltyMod.Settings.inheritanceEnabled == false ||
+                (selPawnTitle = selPawn.royalty?.GetCurrentTitleInFaction(Faction.OfPlayer)?.def) == null ||
+                !selPawnTitle.canBeInherited) yield break;
+
+            //Define action to run when selected
+            void SetHeirAction(LocalTargetInfo targetinfo)
             {
-                //Define action to run when select`ed
-                void SetHeirAction(LocalTargetInfo targetinfo)
-                {
-	                var targetPawnTitle = targetinfo.Pawn.royalty.GetCurrentTitle(Faction.OfPlayer);
+	            var targetPawnTitle = targetinfo.Pawn.royalty.GetCurrentTitle(Faction.OfPlayer);
 
-	                if (targetPawnTitle == null || targetPawnTitle.seniority < selPawnTitle.seniority)
-	                {
-		                selPawn.royalty.SetHeir(targetinfo.Pawn, Faction.OfPlayer);
-		                Messages.Message("TR_setheir_success".Translate(selPawn.Name.ToStringShort, targetinfo.Pawn.Name.ToStringShort), MessageTypeDefOf.NeutralEvent);
-	                }
-	                else
-	                {
-		                Messages.Message("TR_setheir_failed_sameorhighertitle".Translate(targetinfo.Pawn.Name.ToStringShort), MessageTypeDefOf.RejectInput);
-	                }
-                }
-
-                //Target pawn for selection
-                var setHeirOption = new FloatMenuOption("TR_Command_setheir_label".Translate(selPawn.Name.ToStringShort), delegate
-                {
-                    Find.Targeter.BeginTargeting(TargetingParameters.ForColonist(), SetHeirAction);
-                });
-
-                //Return the float option
-                yield return setHeirOption;
+	            if (targetPawnTitle == null || targetPawnTitle.seniority < selPawnTitle.seniority)
+	            {
+		            selPawn.royalty.SetHeir(targetinfo.Pawn, Faction.OfPlayer);
+		            Messages.Message(
+			            "TR_setheir_success".Translate(selPawn.Name.ToStringShort, targetinfo.Pawn.Name.ToStringShort),
+			            MessageTypeDefOf.NeutralEvent);
+		            
+	            }
+	            else
+	            {
+		            Messages.Message("TR_setheir_failed_sameorhighertitle".Translate(targetinfo.Pawn.Name.ToStringShort), MessageTypeDefOf.RejectInput);
+	            }
             }
+
+            //Target pawn for selection
+            var setHeirOption = new FloatMenuOption("TR_Command_setheir_label".Translate(selPawn.Name.ToStringShort),
+	            delegate { Find.Targeter.BeginTargeting(TargetingParameters.ForColonist(), SetHeirAction); });
+
+            //Return the float option
+            yield return setHeirOption;
         }
     }
 }
