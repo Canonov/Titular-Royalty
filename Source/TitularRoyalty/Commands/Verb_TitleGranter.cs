743b2e4e6f13ac96298a5e322f063e3db834578c
CanonOverseer <agent6841@gmail.com>
2023-08-25 13:54:36 +0800

    Update Verb_TitleGranter.cs

--- Source/TitularRoyalty/Commands/Verb_TitleGranter.cs
+++ Source/TitularRoyalty/Commands/Verb_TitleGranter.cs
@@ -16,7 +16,7 @@ namespace TitularRoyalty
 
         public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true) => true;
 
-        protected override bool TryCastShot()
+        public override bool TryCastShot()
 		{
 			if (!(currentTarget.Thing is Pawn) || currentTarget.Pawn.royalty == null) return false;
 			
