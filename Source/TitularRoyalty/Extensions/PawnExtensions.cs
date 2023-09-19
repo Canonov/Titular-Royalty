7184260b56ba255b8c2f04f5a1f1a62ee9e0089f
CanonOverseer <agent6841@gmail.com>
2023-09-19 21:05:21 +0800

    Create PawnExtensions.cs

---
+++ Source/TitularRoyalty/Extensions/PawnExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Verse;
+
+namespace TitularRoyalty.Extensions
+{
+    public static class PawnExtensions
+    {
+        public static Dictionary<Pawn, Comp_PlayerRoyaltyTracker> CachedPawnComps { get; private set; } =
+            new Dictionary<Pawn, Comp_PlayerRoyaltyTracker>();
+        
+        /// <summary>
+        /// Gets the cached royalty tracker, or finds and caches it.
+        /// </summary>
+        /// <param name="pawn"></param>
+        /// <returns>the Pawn's PlayerRoyaltyTracker</returns>
+        public static Comp_PlayerRoyaltyTracker PlayerRoyalty(this Pawn pawn)
+        {
+            if (CachedPawnComps.TryGetValue(pawn, out var royaltyTracker))
+            {
+                return royaltyTracker;
+            }
+
+            royaltyTracker = pawn.GetComp<Comp_PlayerRoyaltyTracker>();
+            CachedPawnComps.Add(pawn, royaltyTracker);
+
+            return royaltyTracker;
+        }
+
+        /// <summary>
+        /// Check if the pawn has any player titles
+        /// </summary>
+        /// <returns>Whether the pawn has any titles</returns>
+        public static bool HasAnyPlayerTitle(this Pawn pawn)
+        {
+            return pawn.PlayerRoyalty().HasAnyTitle;
+        }
+    }
+}
