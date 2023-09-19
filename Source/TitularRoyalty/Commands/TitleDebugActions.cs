581ccbbd5fae158d6a47b227f1a7816bab5e4cb9
CanonOverseer <agent6841@gmail.com>
2023-09-19 21:05:24 +0800

    Create TitleDebugActions.cs

---
+++ Source/TitularRoyalty/Commands/TitleDebugActions.cs
@@ -0,0 +1,24 @@
+﻿using TitularRoyalty.Titles;
+using Verse;
+
+namespace TitularRoyalty.Commands
+{
+    public static class TitleDebugActions
+    {
+        [DebugAction("Titular Royalty", "List Titles", actionType = DebugActionType.Action)]
+        private static void ListTitles()
+        {
+            Log.Message("Listing Titles");
+            GameComponent_PlayerTitlesManager.Current.Titles.ForEach(x => Log.Message(x.label));
+        }
+        
+        [DebugAction("Titular Royalty", "Add Title", actionType = DebugActionType.Action)]
+        private static void AddTitle()
+        {
+            GameComponent_PlayerTitlesManager.Current.AddTitle(new PlayerTitleData()
+            {
+                label = "title_" + Rand.Range(0, 10000), 
+            });
+        }
+    }
+}
