e13e735cef926b0adac28a3a1f494f9fb28a4eaf
CanonOverseer <agent6841@gmail.com>
2023-09-20 01:34:33 +0800

    Create TitleDebugActions.cs

---
+++ Source/TitularRoyalty/Miscellaneous/TitleDebugActions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using TitularRoyalty.Extensions;
+using TitularRoyalty.Titles;
+using Verse;
+
+namespace TitularRoyalty
+{
+    public static class TitleDebugActions
+    {
+        [DebugAction("Titular Royalty", "List Titles", actionType = DebugActionType.Action)]
+        private static void ListTitles()
+        {
+            Log.Message("Listing Titles");
+            GameComponent_PlayerTitlesManager.Current.Titles.ForEach(x => Log.Message(x.label));
+        }
+        
+        [DebugAction("Titular Royalty", "Add Title", actionType = DebugActionType.Action)]
+        private static void AddTitle()
+        {
+            GameComponent_PlayerTitlesManager.Current.AddTitle(new PlayerTitleData()
+            {
+                label = "title_" + Rand.Range(0, 10000), 
+            });
+        }
+        
+        [DebugAction("Titular Royalty", "Grant Title", actionType = DebugActionType.ToolMapForPawns)]
+        private static void GrantTitle()
+        {
+            var randomPawnRoyalty = Find.AnyPlayerHomeMap.mapPawns.FreeColonists.RandomElement().PlayerRoyalty();
+            
+            var debugOptions = new List<DebugMenuOption>();
+            
+            foreach (var title in GameComponent_PlayerTitlesManager.Current.Titles)
+            {
+                debugOptions.Add(new DebugMenuOption(title.label, DebugMenuOptionMode.Action, () =>
+                {
+                    randomPawnRoyalty.SetTitle(title);
+                }));
+            }
+            
+            if (debugOptions.Any())
+            {
+                Find.WindowStack.Add(new Dialog_DebugOptionListLister(debugOptions));
+            }
+            
+        }
+    }
+}
