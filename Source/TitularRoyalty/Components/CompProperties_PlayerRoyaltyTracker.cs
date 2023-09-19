b8eeb1ae3fb68c1d1321cebe892866e7820e353f
CanonOverseer <agent6841@gmail.com>
2023-09-19 15:03:20 +0800

    Royalty Tracker Comp

---
+++ Source/TitularRoyalty/Components/CompProperties_PlayerRoyaltyTracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Verse;
+
+namespace TitularRoyalty
+{
+    /// <summary>
+    /// Not really suppose to have any fields, just a way to link the Comp to the properties and make sure it's only on pawns
+    /// </summary>
+    [UsedImplicitly]
+    public class CompProperties_PlayerRoyaltyTracker : CompProperties
+    {
+        public CompProperties_PlayerRoyaltyTracker()
+        {
+            compClass = typeof(Comp_PlayerRoyaltyTracker);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            // Must be Pawn or Subclass of (just incase)
+            var thingClass = parentDef.thingClass;
+            if (thingClass != typeof(Pawn) && !thingClass.IsSubclassOf(typeof(Pawn)))
+            {
+                yield return "Comp_PlayerRoyaltyTracker can only be applied to Pawns";
+            }
+        }
+    }
+}
---
+++ Source/TitularRoyalty/Components/Comp_PlayerRoyaltyTracker.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace TitularRoyalty
+{
+    
+    /// <summary>
+    /// Applied to Pawns to track their royalty status
+    /// </summary>
+    public class Comp_PlayerRoyaltyTracker : ThingComp
+    {
+        public CompProperties_PlayerRoyaltyTracker Props => (CompProperties_PlayerRoyaltyTracker)props;
+        public Pawn Pawn => (Pawn)parent;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+        }
+    }
+}
