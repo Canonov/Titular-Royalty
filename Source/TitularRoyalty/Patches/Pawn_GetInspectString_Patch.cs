969f6dbe287297f30ff613632a86dd2c4646dc9d
CanonOverseer <agent6841@gmail.com>
2023-09-20 12:59:14 +0800

    Remove debug logs from Pawn_GetInspectString_Patch
    The Pawn_GetInspectString_Patch had numerous debug Log.Message calls scattered across the implementation that are no longer required.

--- Source/TitularRoyalty/Patches/Pawn_GetInspectString_Patch.cs
+++ Source/TitularRoyalty/Patches/Pawn_GetInspectString_Patch.cs
@@ -28,20 +28,15 @@ namespace TitularRoyalty.Patches
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             // todo: skip the original title label if there's going to be a player one 
-            
-            Log.Message("Begin");
             var insertionIndex = -1;
 
             var codeInstructions = instructions.ToList();
-            Log.Message("Instruction ToList");
 
             // run after: stringBuilder.AppendLine(MainDesc(writeFaction: true));
             for (int i = 0; i < codeInstructions.Count; i++)
             {
-                Log.Message($"Instruction[{i}] = {codeInstructions[i].opcode}");
                 if (codeInstructions[i].opcode == OpCodes.Pop)
                 {
-                    Log.Message($"Instruction found");
                     insertionIndex = i + 1;
                     break;
                 }
@@ -49,21 +44,15 @@ namespace TitularRoyalty.Patches
 
             var instructionsToInsert = new List<CodeInstruction>();
             
-            Log.Message($"Inserting at {insertionIndex}, current value: {codeInstructions[insertionIndex].opcode}");
-
-            Log.Message("Ldloc_0");
             instructionsToInsert.Add(new CodeInstruction(OpCodes.Ldloc_0)); // Load stringbuilder
-            Log.Message("Ldarg_0");
             instructionsToInsert.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load pawn
 
-            Log.Message("Call");
             instructionsToInsert.Add(new CodeInstruction(OpCodes.Call,
                 AccessTools.Method(typeof(Pawn_GetInspectString_Patch), nameof(AddTitleInspectString))));
             
 
 
             // Add the new instructions
-            Log.Message("Inserting");
             codeInstructions.InsertRange(insertionIndex, instructionsToInsert);
             return codeInstructions.AsEnumerable();
         }
