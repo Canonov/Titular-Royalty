86a15cf8774a0802b7730108d3bc40b7041e6c75
CanonOverseer <agent6841@gmail.com>
2023-09-20 12:58:02 +0800

    Add documentation to TitleUIUtility class
    The adds a summary comment to the CreateTitlePlateDrawer method in the TitleUIUtility class.

--- Source/TitularRoyalty/UI/TitleUIUtility.cs
+++ Source/TitularRoyalty/UI/TitleUIUtility.cs
@@ -11,6 +11,9 @@ namespace TitularRoyalty.UI
     public static class TitleUIUtility
     {
         
+        /// <summary>
+        /// Returns an action to draw a title plate for the given pawn and title, used in the character card
+        /// </summary>
         public static Action<Rect> CreateTitlePlateDrawer(Pawn pawn, PlayerTitle title) => delegate(Rect holderRect)
         {
             string titleLabel = title.GetLabelForHolder();
