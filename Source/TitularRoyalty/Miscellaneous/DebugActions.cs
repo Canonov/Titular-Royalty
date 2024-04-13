0cdf8cb7a9a241fb384c41b86ca2355a8e76a352
CanonOverseer <agent6841@gmail.com>
2024-04-13 10:09:26 +0800

    Update DebugActions.cs

--- Source/TitularRoyalty/Miscellaneous/DebugActions.cs
+++ Source/TitularRoyalty/Miscellaneous/DebugActions.cs
@@ -7,66 +7,57 @@ namespace TitularRoyalty
 {
     public static class DebugActions
     {
-        [DebugAction("Mods", "TR: Edit Title", false, false, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        [DebugAction("Titular Royalty", "TR: Edit Title", allowedGameStates = AllowedGameStates.PlayingOnMap)]
         public static void EditTitle()
         {
-            var list = new List<DebugMenuOption>();
-            foreach (var title in Faction.OfPlayer.def.RoyalTitlesAllInSeniorityOrderForReading
-                         .OfType<PlayerTitleDef>())
-            {
-                list.Add(new DebugMenuOption($"{title.GetLabelForBothGenders()}", DebugMenuOptionMode.Action, delegate
+            var options = DefDatabase<PlayerTitleDef>.AllDefsListForReading
+                .Select(title => new DebugMenuOption($"{title.GetLabelForBothGenders()}", DebugMenuOptionMode.Action,
+                    delegate
                     {
                         var comp = Current.Game.GetComponent<GameComponent_TitularRoyalty>();
                         Find.WindowStack.Add(new Dialog_RoyalTitleEditor(comp, title, null));
-                    }
-                ));
-            }
+                    }))
+                .ToList();
 
-            Find.WindowStack.Add(new Dialog_DebugOptionListLister(list));
+            Find.WindowStack.Add(new Dialog_DebugOptionListLister(options));
         }
 
-        [DebugAction("Mods", "TR: Reset Custom Titles", false, false,
+        [DebugAction("Titular Royalty", "TR: Reset Custom Titles",
             allowedGameStates = AllowedGameStates.PlayingOnMap)]
         public static void TryResetCustomTitles()
         {
             Current.Game.GetComponent<GameComponent_TitularRoyalty>().ResetTitles();
         }
 
-        [DebugAction("Mods", "TR: Refresh Titles", false, false, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        [DebugAction("Titular Royalty", "TR: Refresh Titles", allowedGameStates = AllowedGameStates.PlayingOnMap)]
         public static void UpdateTitles()
         {
             Current.Game.GetComponent<GameComponent_TitularRoyalty>().SetupTitles();
         }
 
-        [DebugAction("Mods", "TR: Try Apply ModSettings", false, false, allowedGameStates = AllowedGameStates.Playing)]
+        [DebugAction("Titular Royalty", "Try Apply ModSettings", allowedGameStates = AllowedGameStates.Playing)]
         public static void ReloadSettings()
         {
             StartupSetup.ApplyModSettings();
         }
 
-        [DebugAction("Mods", "TR: Export Titlelist to Doc", false, false,
+        [DebugAction("Titular Royalty", "Export Titlelist to Doc",
             allowedGameStates = AllowedGameStates.PlayingOnMap)]
         public static void ExportTitlesToDoc()
         {
-            string doc = "";
+            var doc = new StringBuilder();
 
             foreach (var title in DefDatabase<PlayerTitleDef>.AllDefsListForReading)
             {
-                doc += $"  <li> <!--{title.originalTitleFields.label.CapitalizeFirst()}-->\n";
-                doc += $"    <titleDef>{title.defName}</titleDef>\n";
-                doc += $"    \n";
-                doc += $"    <label>{title.label}</label>\n";
-                doc += $"    <labelFemale>{title.labelFemale ?? "None"}</labelFemale>\n";
-                doc += $"  </li>\n\n";
+                doc.AppendLine($"  <li> <!--{title.originalTitleFields.label.CapitalizeFirst()}-->");
+                doc.AppendLine($"    <titleDef>{title.defName}</titleDef>");
+                doc.AppendLine($"    <label>{title.label}</label>");
+                doc.AppendLine($"    <labelFemale>{title.labelFemale ?? "None"}</labelFemale>");
+                doc.AppendLine($"  </li>");
             }
 
-            using (FileStream fs = File.Create($"RealmTypeList{Rand.Int}.xml"))
-            {
-                byte[] info = new UTF8Encoding(true).GetBytes(doc);
-                fs.Write(info, 0, info.Length);
-            }
-
-            Log.Message($"Saved to your rimworld folder.");
+            File.WriteAllText($"RealmTypeList-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}.xml", doc.ToString());
+            Log.Message("Saved to your rimworld folder.");
         }
     }
 }
