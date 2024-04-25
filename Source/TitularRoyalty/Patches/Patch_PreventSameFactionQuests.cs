cb0122e8d3d98140167f38e4316e56ca149430cc
CanonOverseer <agent6841@gmail.com>
2024-04-25 19:45:04 +0800

    Utilize Harmony Patch Categories

--- Source/TitularRoyalty/Patches/Patch_PreventSameFactionQuests.cs
+++ Source/TitularRoyalty/Patches/Patch_PreventSameFactionQuests.cs
@@ -1,10 +1,14 @@
-﻿using RimWorld.QuestGen;
+﻿using HarmonyLib;
+using RimWorld.QuestGen;
 
-namespace TitularRoyalty;
+namespace TitularRoyalty.Patches;
 
-public static class QuestGen_Patches
+[HarmonyPatch]
+public static class Patch_PreventSameFactionQuests
 {
-    public static void IsGoodPawn_Postfix(ref bool __result, QuestNode_GetPawn __instance, Pawn pawn, Slate slate)
+    [HarmonyPatch(typeof(QuestNode_GetPawn), "IsGoodPawn")]
+    [HarmonyPostfix]
+    private static void IsGoodPawn_Postfix(ref bool __result, QuestNode_GetPawn __instance, Pawn pawn, Slate slate)
     {
         // May have to change this if it pops up in events with other conditions but it seems hospitality_joiners is the only one with problems and this is what that uses
         if (__instance.mustHaveRoyalTitleInCurrentFaction.GetValue(slate)) 
---
+++ Source/TitularRoyalty/Patches/Patch_RoyalTitleIcons.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TitularRoyalty.Patches;
+
+[HarmonyPatchCategory("VFEEmpire")]
+[HarmonyPatch(typeof(Widgets), nameof(Widgets.DefIcon))]
+public static class Patch_RoyalTitleIcons
+{
+	private static bool Prefix(Rect rect, Def def, float scale = 1f)
+	{
+		if (def is not PlayerTitleDef titleDef) 
+			return true;
+		
+		var icon = Resources.GetTitleIcon(titleDef, GameComponent_TitularRoyalty.Current);
+		
+		if (icon != null)
+		{
+			Widgets.DrawTextureFitted(rect, icon, scale);
+			return false;
+		}
+
+		Log.Message("Titular Royalty: Could not find icon for " + titleDef.defName + " in DefIcon_RoyalIconsPrefix.");
+		return true;
+	}
+}
