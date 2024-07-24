81aa36145deee1a479d73c1c06989199eaf95f27
Canon <agent6841@gmail.com>
2024-07-24 12:01:44 +0800

    Update RomanNumerals.cs

--- Source/TitularRoyalty/Miscellaneous/RomanNumerals.cs
+++ Source/TitularRoyalty/Miscellaneous/RomanNumerals.cs
@@ -1,15 +1,16 @@
 ﻿using System.Text;
 
-namespace TitularRoyalty;
-
-public static class RomanNumerals
+namespace TitularRoyalty
 {
-    public static readonly Dictionary<char, int> RomanNumberDictionary;
-    public static readonly Dictionary<int, string> NumberRomanDictionary;
-
-    static RomanNumerals()
+    /// <summary>
+    /// Provides methods to convert between integers and Roman numerals.
+    /// </summary>
+    [PublicAPI]
+    public static class RomanNumerals
     {
-        RomanNumberDictionary = new Dictionary<char, int>
+        
+        // Value of each roman numeral
+        private static readonly Dictionary<char, int> RomanNumeralValues = new()
         {
             { 'I', 1 },
             { 'V', 5 },
@@ -17,10 +18,11 @@ public static class RomanNumerals
             { 'L', 50 },
             { 'C', 100 },
             { 'D', 500 },
-            { 'M', 1000 },
+            { 'M', 1000 }
         };
 
-        NumberRomanDictionary = new Dictionary<int, string>
+        // Dictionary to map integer values to their Roman numeral strings.
+        private static readonly Dictionary<int, string> IntToRoman = new()
         {
             { 1000, "M" },
             { 900, "CM" },
@@ -34,48 +36,54 @@ public static class RomanNumerals
             { 9, "IX" },
             { 5, "V" },
             { 4, "IV" },
-            { 1, "I" },
+            { 1, "I" }
         };
-    }
 
-    public static string FromInt(int number)
-    {
-        var roman = new StringBuilder();
-
-        foreach (var item in NumberRomanDictionary)
+        /// <summary>
+        /// Converts an integer to its Roman numeral representation.
+        /// </summary>
+        /// <param name="number">The integer to convert.</param>
+        /// <returns>The Roman numeral representation of the integer.</returns>
+        public static string FromInt(int number)
         {
-            while (number >= item.Key)
+            var result = new StringBuilder();
+            foreach (var (romanValue, romanChar) in IntToRoman)
             {
-                roman.Append(item.Value);
-                number -= item.Key;
+                while (number >= romanValue)
+                {
+                    result.Append(romanChar);
+                    number -= romanValue;
+                }
             }
+            return result.ToString();
         }
 
-        return roman.ToString();
-    }
-
-    public static int ToInt(string roman)
-    {
-        int total = 0;
-        char previousRoman = '\0';
-
-        foreach (var currentRoman in roman)
+        /// <summary>
+        /// Converts a Roman numeral string to its integer representation.
+        /// </summary>
+        /// <param name="roman">The Roman numeral string to convert.</param>
+        /// <returns>The integer representation of the Roman numeral.</returns>
+        public static int ToInt(string roman)
         {
-            var previous = previousRoman != '\0' ? RomanNumberDictionary[previousRoman] : '\0';
-            var current = RomanNumberDictionary[currentRoman];
+            int total = 0;
+            int previousValue = 0;
 
-            if (previous != 0 && current > previous)
+            foreach (var ch in roman)
             {
-                total = total - (2 * previous) + current;
-            }
-            else
-            {
-                total += current;
+                int currentValue = RomanNumeralValues[ch];
+                total += currentValue;
+                
+                // If the current value is greater than the previous value, we need to correct the total
+                // by subtracting twice the previous value (once to undo the addition, and once to apply the correct subtraction)
+                if (currentValue > previousValue)
+                {
+                    total -= 2 * previousValue;
+                }
+
+                previousValue = currentValue;
             }
 
-            previousRoman = currentRoman;
+            return total;
         }
-
-        return total;
     }
 }
