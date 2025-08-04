c1d69ed4752be8411532d554a9bc25bd96814e88
Canon <canonoverseer@gmail.com>
2025-08-04 17:12:54 +0800

    Hack for Nice Bill Tab

---
+++ Source/TitularRoyalty/Patches/Patch_ThingDef_IsRangedWeapon.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable InconsistentNaming
+
+namespace TitularRoyalty.Patches;
+
+[HarmonyPatch(typeof(ThingDef), nameof(ThingDef.IsRangedWeapon), MethodType.Getter)]
+public static class Patch_ThingDef_IsRangedWeapon
+{
+    // Hack fix for NiceBillTab
+    private static bool Prefix(ThingDef __instance, ref bool __result)
+    {
+        if (__instance.defName == "TitularRoyalty_Staff")
+        {
+            __result = false;
+            return false;
+        }
+
+        return true;
+    }
+}
